95542a7a7be713d38796c4f9d956b84a9d499e6b
mailoForest <kaloyanpetrov1994@gmail.com>
2016-09-02 07:21:47 +0300

    Update demo.php

--- store-task/demo.php
+++ store-task/demo.php
@@ -4,7 +4,8 @@ use Product\Product;
 use Product\Bundle;
 require_once 'autoload.php';
 
-$vaflaMura = new Product("vafla mura", '0.4', 100);
+$vaflaMura = new Product("vafla mura", 0.4, 100);
+$biskviti = new Product("biskviti", 1.4, 80);
 
 $bread = new Product('bread', 1, 20);
 $milk = new Product('milk', 2, 50);
@@ -15,8 +16,18 @@ $vaflaMura->sell(10);
 $vaflaMura->load(5);
 
 
-$popara = new Bundle('popara', 1);
+$popara = new Bundle('popara', 5);
 $popara->addProduct($bread, 1);
-$popara->addProduct($milk, 0.5);
+$popara->addProduct($milk, 1);
+$popara->addProduct($sugar, 0.5);
 
-var_dump($popara);
+$zakuska = new Bundle('zakuska', 3);
+$zakuska->addProduct($biskviti, 1);
+$zakuska->addProduct($milk, 1);
+
+$naslada = new Bundle('naslada', 5);
+$naslada->addProduct($vaflaMura, 3);
+$naslada->addProduct($biskviti, 2);
+$naslada->addProduct($sugar, 1);
+
+var_dump($popara); var_dump($zakuska); var_dump($naslada);
